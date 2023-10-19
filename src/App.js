511a18478e76023c70ad2e16e2a6f4de1ff8771e
Arulchinnapparaj <arulchinnapparaj1@gmail.com>
2023-10-19 01:59:51 -0700

    Arul

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Leo } from './Manoj/Leo';
+import { Interaction } from './Arul/Interaction';
+import { Tshirt } from './Arul/Tshirt';
+import { NEWARRIVALS, Newarr } from './Arul/NEWARRIVALS';
 
 function App() {
   return (
@@ -8,6 +11,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Leo />} />
+          
         </Routes>
       </Router>
     </div>
