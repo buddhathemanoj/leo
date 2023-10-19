b529633b3c6592da2fb0ade8f67d6e389bab8f55
buddhathemanoj <prabhakarmanoj743@gmail.com>
2023-10-19 07:08:00 +0530

    Leo

---
+++ src/Manoj/Leo.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+export const Leo=()=>{
+    return(
+        <div>
+            <h1>LEO</h1>
+            <NavLink to="/about">About</NavLink>
+        </div>
+    )
+}
