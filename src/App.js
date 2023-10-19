76b11107f22ae25051fc0d94c58d28dec09bd120
manojprabha2k <prabhakarlive2000@gmail.com>
2023-10-19 02:09:34 -0700

    Renuka

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Leo } from './Manoj/Leo';
-import { Design } from './Renuka/Design';
+
 
 function App() {
   return (
